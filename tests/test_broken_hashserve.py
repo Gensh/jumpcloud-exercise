b83f2b38a816b4b388f67931810e1bbbbca875ac
Gensh <saintgensh@gmail.com>
2021-03-01 23:21:44 -0500

    GET tests borked draft

--- tests/test_broken_hashserve.py
+++ tests/test_broken_hashserve.py
@@ -14,18 +14,24 @@ base_path = pathlib.Path().absolute()
 hashserve_exe = base_path / "broken_hashserve" / f"broken-hashserve_{hashserve_suffix}"
 
 
+good_passwords = [
+    ("abc123", "YWJjMTIz"),
+]
+
+
 def run_curl(request, endpoint):
     """Run a curl request which plays by the rules."""
 
     command = f"curl {request} http://127.0.0.1:8088/{endpoint}"
-    return subprocess.run(command, text=True, check=True)
+    return subprocess.run(command, text=True)
 
 
-@pytest.fixture(scope="class", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def start_server():
     """
     Start the server. Opens the process and does nothing.
-    Will autorun for each test class in this module.
+    Will autorun for each test for the sake of keeping results independent.
+     Could be moved back to class-based if index-based collisions could be avoided.
     No need to test startup since that's implicit for every other test.
     :return:
     """
@@ -33,19 +39,54 @@ def start_server():
     subprocess.Popen(hashserve_exe)
 
 
-class HashServeHashCrudTest:
+class HashServeGetTest:
     """
-    Test class for regular hashserve Create/Read/Update/Delete.
+    Tests for GET requests.
     """
 
     @staticmethod
-    def test_get_base64_password():
-        """Test getting the """
+    @pytest.mark.parametrize("password, encoded", good_passwords)
+    def test_get_good_password(password, encoded):
+        """
+        When I send a GET request
+        I want to get the encoded password
+        So I can do my security thing
+        :param password: A known good password.
+        :param encoded: The base64 encoding of that password.
+        """
 
+        # Put the password in.
+        json_object = f"""{{"password":"{password}"}}"""
+        run_curl(
+            request=f"""-X POST -H "application/json" -d '{json_object}'""",
+            endpoint="hash",
+        )
+
+        # Get the encoded version out.
         result = run_curl(request='-H "application/json"', endpoint="hash/1")
-        ciphertext = "zHkbvZDdwYYiDnwtDdv/FIWvcy1sKCb7qi7Nu8Q8Cd/MqjQeyCI0pWKDGp74A1g=="
-        assert result.stdout == ciphertext, (
+        assert result.returncode == 200, (
+            "Received a return code other than 200" f"\nActual:   {result.returncode}"
+        )
+        assert result.stdout == encoded, (
             "Failed to get the correct base64 password"
             f"\nActual:   {result.stdout}"
-            f"\nExpected: {ciphertext}"
+            f"\nExpected: {encoded}"
+        )
+
+    @staticmethod
+    def test_fail_if_empty():
+        """
+        When I send a GET request to an empty database
+        I want to receive an error code
+        So I can readily recognize the issue
+        """
+
+        result = run_curl(request='-H "application/json"', endpoint="hash/1")
+        assert result.returncode != 200, "Received code 200 when in error state"
+        assert (
+            result.returncode != 0
+        ), "Did not receive a return code when in error state"
+        assert result.stdout is None, (
+            "Found a password when expecting an empty database"
+            f"\nActual:   {result.stdout}"
         )
