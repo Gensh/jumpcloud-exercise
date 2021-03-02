e43da21279984bc52cc73ac7dd7cb0a1bdd8e993
Gensh <saintgensh@gmail.com>
2021-03-01 19:50:59 -0500

    Spin up server

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,12 +1,29 @@
 """Test module"""
 
+import pathlib
+import platform
+import subprocess
+
+import pytest
+
+my_os = platform.system()
+hashserve_suffix = "win.exe" if my_os == "Windows" else my_os.lower()
+base_path = pathlib.Path().absolute()
+hashserve_exe = base_path / "broken_hashserve" / f"broken-hashserve_{hashserve_suffix}"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def start_server():
+    return subprocess.run(hashserve_exe)
+
 
 class ATest:
     """
     ATest
     """
 
-    def test_a(self):
+    @staticmethod
+    def test_a():
         """test_a"""
 
-        pass
+        raise RuntimeError
