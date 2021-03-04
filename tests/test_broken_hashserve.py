86352ecf0f724abccd4a4d23b727f6ee8ec3cda4
Gensh <saintgensh@gmail.com>
2021-03-04 17:07:09 -0500

    Add init to root and scenario in tests

--- tests/test_broken_hashserve.py
+++ tests/test_broken_hashserve.py
@@ -11,6 +11,28 @@ good_passwords = [
 ]
 
 
+"""
+- When launched, the application should wait for http connections.
+- It should answer on the `PORT` specified in the `PORT` environment variable.
+- It should support three endpoints:
+    - A `POST` to `/hash` should accept a password.
+      It should return a job identifier immediately.
+      It should then wait 5 seconds and compute the password hash.
+      The hashing algorithm should be SHA512.
+    - A `GET` to `/hash` should accept a job identifier.
+      It should return the base64 encoded password hash for the corresponding
+      `POST` request.
+    - A `GET` to `/stats` should accept no data.
+      It should return a JSON data structure for the total hash requests since
+      the server started and the average time of a hash request in milliseconds.
+- The software should be able to process multiple connections simultaneously.
+- The software should support a graceful shutdown request. Meaning, it should
+  allow any in-flight password hashing to complete, reject any new requests,
+  respond with a `200` and shutdown.
+- No additional password requests should be allowed when shutdown is pending.
+"""
+
+
 class HashServeGetTest:
     """
     Tests for GET requests.
