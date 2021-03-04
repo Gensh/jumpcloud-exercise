615041d1363029043675f0e73e510cd1387f386c
Gensh <saintgensh@gmail.com>
2021-03-04 17:00:55 -0500

    Added missing subprocess.run param

--- tests/cmd_util.py
+++ tests/cmd_util.py
@@ -5,4 +5,4 @@ def run_curl(request, endpoint):
     """Run a curl request which plays by the rules."""
 
     command = f"curl {request} http://127.0.0.1:8088/{endpoint}"
-    return subprocess.run(command, text=True)
+    return subprocess.run(command, capture_output=True, text=True)
