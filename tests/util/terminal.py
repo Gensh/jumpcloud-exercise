a71318d646ece535bbbcb856ab83a66fc9928a28
Gensh <saintgensh@gmail.com>
2021-03-04 19:47:01 -0500

    Add stats and shutdown utils

--- tests/util/terminal.py
+++ tests/util/terminal.py
@@ -13,7 +13,13 @@ def run_curl(request, endpoint):
 
 
 def post_password(password):
-    """POST the password to the hash endpoint."""
+    """
+    POST the password to the hash endpoint.
+
+    $ curl -X POST -H "application/json" -d '{"password":"angrymonkey"}'
+     http://127.0.0.1:8088/hash
+    > 42
+    """
 
     json_object = f"""{{"password":"{password}"}}"""
     return run_curl(
@@ -27,6 +33,9 @@ def get_result(index):
     GET the given encoded password hash.
     Correctly, it should be a 1-index integer, but we won't restrict it here
      in order to allow failure testing.
+
+    $ curl -H "application/json" http://127.0.0.1:8088/hash/1
+    > zHkbvZDdwYYiDnwtDdv/FIWvcy1sKCb7qi7Nu8Q8Cd/MqjQeyCI0pWKDGp74A1g==
     """
 
     return run_curl(request='-H "application/json"', endpoint=f"hash/{index}")
@@ -36,3 +45,25 @@ def get_first_result():
     """GET the first encoded password hash."""
 
     return get_result(1)
+
+
+def get_stats():
+    """
+    GET the stats.
+
+    $ curl http://127.0.0.1:8088/stats
+    {"TotalRequests":3,"AverageTime":5004625}
+    """
+
+    return run_curl(request="", endpoint="stats")
+
+
+def shutdown():
+    """
+    POST the shutdown command.
+
+    $ curl -X POST -d 'shutdown' http://127.0.0.1:8088/hash
+    > 200 Empty Response
+    """
+
+    return run_curl(request="-X POST -d 'shutdown'", endpoint="hash")
